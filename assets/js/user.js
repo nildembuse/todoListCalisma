2449a776c79f70dca004ab8bb539cd085686deab
Nildem Buse Ertürk <nildembuse@gmail.com>
2023-08-20 14:16:34 +0300

    html ve js de duzenleme yapildi

---
+++ assets/js/user.js
@@ -0,0 +1,32 @@
+const urlPrefix = 'https://todorestapi-20432433159e.herokuapp.com/api/users'
+
+const requesUrl = urlPrefix + 'users';
+const requesCretaUrl = urlPrefix + 'creta';
+let users = [];
+
+fetch(urlPrefix)
+      .then(response => response.json())
+      .then (json => {
+        users = json;
+
+      });
+
+
+async function loadDetailPage(usersId) {
+    const usersDetail = await fetch(urlPrefix + 'users/' + todosId).then(x => x.json());
+        renderDetailPage(todosDetail, );
+   }
+
+
+fetch('https://todorestapi-20432433159e.herokuapp.com/api/todos/create/', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: string,
+            completed: boolean,
+            actor: actorId,
+            assignee: assigneeId
+        }),
+        headers: {
+            'Content-type': 'application/json;'
+        }
+    });
